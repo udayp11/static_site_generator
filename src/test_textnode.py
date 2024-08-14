b7f6a9c3b41871ff04cc42da26ecba80979e8540
udayp11 <pandeyuday96@gmail.com>
2024-08-14 13:20:03 +0530

    TextNode testing done asserNotEqual

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -15,10 +15,15 @@ class TestTextNode(unittest.TestCase):
         node4 = TextNode("Hello there", "italic")
         self.assertEqual(node3,node4)
 
+    def test_not_equal(self):
+        node5 = TextNode("Namaste", "italic")
+        node6 = TextNode("Hello there", "italic")
+        self.assertNotEqual(node5,node6)
+
     def test_eq3(self):
-        node3 = TextNode("Hello there", "italic","https://www.olympics.com")
-        node4 = TextNode("Hello there", "italic","https://www.olympics.com")
-        self.assertEqual(node3,node4)
+        node7 = TextNode("Hello there", "italic","https://www.olympics.com")
+        node8 = TextNode("Hello there", "italic","https://www.olympics.com")
+        self.assertEqual(node7,node8)
 
 
 if __name__ == "__main__":
